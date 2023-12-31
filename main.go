a1a395a4c1c1c049e3e943aa3bf203aefa995759
abinash <abinashphulkonwar1@gmail.com>
2023-12-31 12:19:21 +0530

    add read from clipboard feature

--- main.go
+++ main.go
@@ -11,14 +11,25 @@ func main() {
 	var text string
 	flag.StringVar(&text, "t", "", "text")
 	flag.Parse()
-	println(text)
 	err := clipboard.Init()
 	if err != nil {
+
 		panic(err)
+
+	}
+
+	if text == "" {
+		text = string(clipboard.Read(clipboard.FmtText))
 	}
 
 	text = strings.ReplaceAll(text, "\n", " ")
-
+	text = strings.ReplaceAll(text, "\r", " ")
+	text = strings.ReplaceAll(text, "\t", " ")
+	text = strings.ReplaceAll(text, "  ", " ")
+	if text[0] == ' ' {
+		text = text[1:]
+	}
 	clipboard.Write(clipboard.FmtText, []byte(text))
+	println("Text copied to clip board")
 
 }
